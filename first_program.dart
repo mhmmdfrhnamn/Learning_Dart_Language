dcecf88fa3074fc29244a54557e376a7dc4eb327
mhmmdfrhnamn <muhammadfarhanamien@gmail.com>
2025-11-24 14:02:50 +0700

    learn variabel and type data

--- first_program.dart
+++ first_program.dart
@@ -1,3 +1,18 @@
 void main(){
   print('Hello World');
+  var name= 'Voyager';
+  var year=2025;
+  var antennaDiameter= 3.7;
+  var objek= ['apel', 'mangga', 'jeruk'];
+  var img= {
+    'tags':['saturn'],
+    'url': '//path/to/saturn/jpg'
+  };
+
+  print(name);
+  print('${name}');
+  print(year);
+  print(antennaDiameter);
+  print(objek);
+  print(img);
 }
