96bce80d9e5e555e309408dbb9354beb84ba6b48
mhmmdfrhnamn <muhammadfarhanamien@gmail.com>
2025-11-25 19:58:58 +0700

    learn variables. small step is better than do nothing

---
+++ variable.dart
@@ -0,0 +1,8 @@
+// Variabel adalah tempat menyimpan data 
+// untuk penulisaanya biasanya pake camelCase exam:firstName
+
+void main(){
+  // Tipe data string
+String name = 'Muhammad Farhan Amien';
+print(name);
+}
