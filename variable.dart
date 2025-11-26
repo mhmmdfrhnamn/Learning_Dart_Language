ec8db0f9110d00acdee789bd53ecf6edd925336c
mhmmdfrhnamn <muhammadfarhanamien@gmail.com>
2025-11-26 19:43:51 +0700

    learn kata kunci var dan beberapa tipe data kayak string number float dll

--- variable.dart
+++ variable.dart
@@ -5,4 +5,12 @@ void main(){
   // Tipe data string
 String name = 'Muhammad Farhan Amien';
 print(name);
+
+// Kata Kunci var = tipe data yang diisi di kata kunci ini akan dibaca otomatis sama sistem
+var buah= 'apel';
+var kamar=10;
+var koma=10.5;
+print(buah);
+print(kamar);
+print(koma);
 }
