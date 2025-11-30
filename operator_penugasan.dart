03f49a732d74ea1b2fa4a2de6d55dc7a21616ab1
mhmmdfrhnamn <muhammadfarhanamien@gmail.com>
2025-11-30 21:58:06 +0700

    learning operator penugasan. small step is better than nothing

---
+++ operator_penugasan.dart
@@ -0,0 +1,22 @@
+void main (){
+  var a=10;
+  a = a+10;
+  // print(a);
+  
+  // cara bisa dipersingkat seperti dibawah ini
+  var b = 20;
+
+  b += 10; 
+  print(b);
+
+  b -= 10; 
+  print(b);
+
+  b ~/= 10;
+  print(b);
+
+  b %= 10; 
+  print(b);
+
+  
+}
