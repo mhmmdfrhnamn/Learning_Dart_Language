41c76813d4f17e56e61606212b48387837b4c015
mhmmdfrhnamn <farhanexcellentz17@gmail.com>
2025-12-08 23:59:34 +0700

    learn lanjutan

--- recursive_function.dart
+++ recursive_function.dart
@@ -2,10 +2,14 @@
  * recursive function adalah function yang memanggil function dirinya sendiri
  * kadang dalam pekerjaan, kita sering menemui kasus dimana menggunakan recursive function
  * lebih mudah dibandingkan tidak menggunakan recursive function 
- * 
+ *
+ * cuma kita perlu hati hati karena jika kita terlalu dalam maka ada kemungkinan
+ * stack overflow
+ *  
  * contoh kasus yang lebih mudah diselesaikan menggunakan recursive adalah factorial
  */
 
+// contoh pake metode for loop
 int factorialLoop(int value){
   var result = 1;
   for(var i = 1; i <= value; i++){
@@ -15,7 +19,17 @@ int factorialLoop(int value){
   return result;
 }
 
+// contoh recursive dan kondisi
+int factorialRecursive(int value){
+  if(value == 1){
+    return 1;
+  }else {
+    return value * factorialRecursive(value-1);
+  }
+}
+
 void main () {
   print(factorialLoop(10));
   print(1*2*3*4*5*6*7*8*9*10);
+  print(factorialRecursive(10)); //fac(10) => 10 * 9 * fac(8)
 }
