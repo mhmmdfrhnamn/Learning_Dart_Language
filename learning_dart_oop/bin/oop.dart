ee42b2d2adc149b38110f0d9440c50843f071ab7
mhmmdfrhnamn <farhanexcellentz17@gmail.com>
2025-12-10 02:49:16 +0700

    learn dart oop. class and object

--- learning_dart_oop/bin/oop.dart
+++ learning_dart_oop/bin/oop.dart
@@ -1,23 +1,18 @@
-class Person{
-  String name = 'anonim';
-  String? alamat;
-  final String negara = 'indonesia'; 
+class Person {
+  String name = 'Guest';
+  String? addres;
+  final String negara = 'indonesia';
 }
 
-void main(){
+void main () {
+
   var person1 = Person();
-  print(person1);
-  print(person1.name);
-  print(person1.alamat);
-  print(person1.negara);
-
   Person person2 = Person();
-  print(person2);
+  Person person3 = Person();
 
-  // Contoh manipulasi field 
-  person2.name = 'Muhammad farhan amien';
-  person2.alamat = 'Kangenan Pamekasan';
-  print(person2.name);
-  print(person2.alamat);
+  print(person1.name);
+  print(person2.addres);
+  print(person3.negara);
 
+ 
 }
