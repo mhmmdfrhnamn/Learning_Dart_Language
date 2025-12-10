de4acd4a315844efe89f055f467ae574134bae28
mhmmdfrhnamn <farhanexcellentz17@gmail.com>
2025-12-11 02:23:12 +0700

    learn method in class

--- learning_dart_oop/bin/oop.dart
+++ learning_dart_oop/bin/oop.dart
@@ -2,6 +2,18 @@ class Person {
   String name = 'Guest';
   String? addres;
   final String negara = 'indonesia';
+
+  void sayHello(String paramName){
+    print('Hello $paramName, my name is $name');
+  }
+
+  void hello(){
+    print("hello saya adalah $name");
+  }
+
+  String getName(String buah){
+    return "hello saya $name"; 
+  }
 }
 
 void main () {
@@ -16,6 +28,10 @@ void main () {
   print(person1.name);
   print(person1.addres);
   print(person1.negara);
+
+  person1.sayHello('Radif');
+  person1.hello();
+  print(person1.getName('anggur'));
 
  
 }
