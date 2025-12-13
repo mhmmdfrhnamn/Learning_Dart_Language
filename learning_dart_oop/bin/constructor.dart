17db7c9dc941f87b7ad5791b394decde08a6d3e6
mhmmdfrhnamn <farhanexcellentz17@gmail.com>
2025-12-13 23:36:08 +0700

    learning constructor

---
+++ learning_dart_oop/bin/constructor.dart
@@ -0,0 +1,33 @@
+/*
+- Constructor adalah method yang akan dipanggil saat pertama
+  kali object dibuat
+- nama constructor harus sama dengan class dan tidak butuh kata 
+  kunci void dan return value kayak function atau method
+- Ketika menambahkan constructor pada class, maka saat membuat object
+  baru  kita wajib mengikuti paramater yang ada di costructor
+ */
+
+class Person {
+
+  String name = 'guest';
+  String? addres;
+  final String country = 'indonesia';
+
+  // cara buat constructor dengan cara lama
+  // Person(String paramName, String paramAddres){
+  //   name = paramName;
+  //   addres = paramAddres;
+  // }
+
+  // cara lebih ringkas
+
+  Person(this.name, this.addres);
+}
+
+void main (){
+
+  var person = Person('Farhan', 'Kangenan');
+  print(person.name);
+  print(person.addres);
+
+}
